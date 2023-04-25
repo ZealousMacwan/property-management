45ecf45404eb89bb5a47861f70dbb19e67c8cb9d
Zealous Macwan <Macwan.Zealous@philips.com>
2023-04-25 12:15:28 +0530

    fixed-few-sonarlint-issues

--- src/main/java/com/zealous/propertymanagement/controller/CalculatorMain.java
+++ src/main/java/com/zealous/propertymanagement/controller/CalculatorMain.java
@@ -4,6 +4,5 @@ public class CalculatorMain {
     public static void main(String[] args){
         CalculatorController cc = new CalculatorController();
         Double result = cc.add(4.4,5.6);
-        System.out.println(result);
     }
 }
