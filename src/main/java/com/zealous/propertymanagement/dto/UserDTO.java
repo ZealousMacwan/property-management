b4eaf5e9d5fff470a0252cdcbe6612925ee6e1f7
Zealous Macwan <Macwan.Zealous@philips.com>
2023-05-04 14:44:06 +0530

    till-chapter-7-aop-error-handling-2

--- src/main/java/com/zealous/propertymanagement/dto/UserDTO.java
+++ src/main/java/com/zealous/propertymanagement/dto/UserDTO.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import javax.persistence.Column;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -12,7 +15,12 @@ import javax.persistence.Column;
 public class UserDTO {
     private Long id;
     private String ownerName;
+    @NotNull(message = "OwnerEmail is mandatory")
+    @NotEmpty(message = "OwnerEmail can not be empty")
+    @Size(min = 1,max = 50, message = "OwnerEmail should be 1 to 50 char in length")
     private String ownerEmail;
     private String mobile;
+    @NotNull(message = "Password can not be null")
+    @NotEmpty(message = "Password can not be empty")
     private String password;
 }
