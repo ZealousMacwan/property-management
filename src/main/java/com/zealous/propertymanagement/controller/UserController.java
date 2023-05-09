51378797e8e273f61f778a9297d513b27341f61e
Zealous Macwan <zealousvmac@gmail.com>
2023-05-09 12:19:03 +0530

    enriching-swagger-api-documentation-with-extra-annotation

--- src/main/java/com/zealous/propertymanagement/controller/UserController.java
+++ src/main/java/com/zealous/propertymanagement/controller/UserController.java
@@ -2,6 +2,9 @@ package com.zealous.propertymanagement.controller;
 
 import com.zealous.propertymanagement.dto.UserDTO;
 import com.zealous.propertymanagement.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,13 +22,16 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Register", description = "Post method for user registration")
     @PostMapping("/register")
-    public ResponseEntity<UserDTO> register(@Valid @RequestBody UserDTO userDTO){
+    public ResponseEntity<UserDTO> register(
+            @ParameterObject UserDTO paramObjUserDTO,
+            @Valid @RequestBody UserDTO userDTO){
         userDTO = userService.register(userDTO);
         return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
     }
 
-    @PostMapping("/login")
+    @PostMapping(path = "/login", produces = "application/json", consumes = "application/json")
     public ResponseEntity<UserDTO> login(@Valid @RequestBody UserDTO userDTO){
         userDTO = userService.login(userDTO.getOwnerEmail(),userDTO.getPassword());
         return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
